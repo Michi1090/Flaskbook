64113628b17bb8970dfe6c2b1c3e538f774d1eec
Michi <michi.m1090@gmail.com>
2022-08-31 21:53:45 +0900

    ADD:chapter5

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,4 +1,8 @@
+from apps.app import db
+from apps.crud.models import User
+from apps.detector.models import UserImage
 from flask import Blueprint, render_template
+
 
 dt = Blueprint(
     'detector',
@@ -8,4 +12,10 @@ dt = Blueprint(
 
 @dt.route('/')
 def index():
-    return render_template('detector/index.html')
+    user_images = (
+        db.session.query(User, UserImage)
+        .join(UserImage)
+        .filter(User.id == UserImage.user_id)
+        .all()
+    )
+    return render_template('detector/index.html', user_images=user_images)
